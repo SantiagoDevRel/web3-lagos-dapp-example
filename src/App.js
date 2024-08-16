fb3420de34517982709b372ce71d6a8e9235ceb6
santiagodevrel <santiago@chainsafe.io>
2024-08-16 17:38:46 +0300

    created react app with chainlink plugin

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Web3 } from "web3";
+import { ChainlinkPlugin, MainnetPriceFeeds } from "@chainsafe/web3-plugin-chainlink";
+import { useState } from "react";
 
 function App() {
+  const [btcPrice, setBtcPrice] = useState("0");
+  const [ethPrice, setEthPrice] = useState("0");
+
+  // Initialize web3 and register plugin
+  const web3 = new Web3(window.ethereum);
+  web3.registerPlugin(new ChainlinkPlugin());
+
+  async function getBtcPrice() {
+    const result = await web3.chainlink.getPrice(MainnetPriceFeeds.BtcUsd);
+    const formattedResult = result.answer.toString().substring(0, 5);
+    setBtcPrice(formattedResult);
+  }
+
+  async function getEthPrice() {
+    const result = await web3.chainlink.getPrice(MainnetPriceFeeds.EthUsd);
+    const formattedResult = result.answer.toString().substring(0, 4);
+    setEthPrice(formattedResult);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={getBtcPrice}>Get BTC Price</button>
+        <p>{btcPrice}$</p>
+        <button onClick={getEthPrice}>Get ETH Price</button>
+        <p>{ethPrice}$</p>
       </header>
     </div>
   );
